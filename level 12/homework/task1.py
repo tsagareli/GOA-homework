b63f84901f20666c737f74ce27600f52a9112f49
tsagareli <tsagarelielene@gmail.com>
2024-08-23 16:37:58 +0400

    commit

---
+++ level 12/homework/task1.py
@@ -0,0 +1,32 @@
+# 2) კომენტარების გამოყენებით ახსენით რა არის string, integer, float
+# 3) კომენტარების გამოყენებით ახსენით რისთვის გვჭირდება ინფორმაციის დაყოფა მონაცემთა ტიპებად
+# 4) დაწერეთ 20 კონკატინაციის (სთრინგების შეერთების) მაგალითი 
+# 5) მომხმარებლისგან მიიღეთ ინფორმაცია input ბრძანების გამოყენებით და შეინახეთ ცვლადში. შემდეგ დაბეჭდეთ ამ ცვლადის მნიშვნელობა
+
+#string არის ბრჭყალებში მოთავსებული სტრინგი
+#integer არის წილადი
+#float არის ათწილადი
+
+#რომ მე გავიგო რა დავწერე და სხვაც მიხვდეს
+
+print("elene " + "tsagareli")
+print("laqi " + "tsagareli")
+print("davit " + "despicable")
+print("jano " + "chad")
+print("bur" + "ger")
+print("xin" + "kali")
+print("lobi" + "ani")
+print("ele" + "ne")
+print("wireli " + "tetri")
+print("razmis" + "lideri")
+print("lascha" + "giorgi")
+print("goa " + "aris " + "magari")
+print("goa " + "is " + "the " + "best")
+print("laqi " + "elene")
+print("tepshi " + "saini")
+print("stoli " + "magida")
+print("gamarjoba " + "hello")
+print("woit " + "glowo")
+
+name = input("enter your name: ")
+print(name + " this is your name")
