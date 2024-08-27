0ecdf18502e4b2dc680348e1da928edf7b341b65
tsagareli <tsagarelielene@gmail.com>
2024-08-27 13:46:26 +0400

    commit

---
+++ level 16/classwork/task0.py
@@ -0,0 +1,32 @@
+print(76 > 9)
+print(10 > 1)
+print(20 > 8)
+print(83 > 0)
+print(1 < 90)
+print(3 < 73)
+print(8 < 100)
+print(6 < 27)
+print(10 == 10)
+print(20 == 20)
+print(50 == 50)
+print(90 == 90)
+print(87 <= 100)
+print(837 <= 832)
+print(747 <= 1038)
+print(373 <= 902)
+print(844 >= 49)
+print(48373 >= 0)
+print(2982 >= 394)
+print(3623 >= 20)
+print(347 != 832)
+print(280 != 83)
+print(3480 != 92)
+print(493 != 2039)
+
+b1 = int(input("enter your 1 number"))
+b2 = int(input("enter your 2 number"))
+print
+print
+print
+print
+print
