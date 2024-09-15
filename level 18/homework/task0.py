a50c53220907859e597425f20ca16d595c178d17
tsagareli <tsagarelielene@gmail.com>
2024-09-15 18:31:01 +0400

    commit

---
+++ level 18/homework/task0.py
@@ -0,0 +1,49 @@
+#  2) ახსენით and ოპერატორი თქვენი სიტყვებით      
+#Returns True if both statements are true	
+
+#  3) ახსენით or ოპერატორი თქვენი სიტყვებით      
+##Returns True if one of the statements is true
+
+#  4) გაამარტივეთ მოცემული კოდი:
+
+# print((True or False) and (True and True))
+print((True) and (True))
+
+# print((True or False) and (False or False))
+print((True) and (False))
+
+# print((False and True) or (True and False))
+print((False) or (False))
+
+# print((False or False) and (True or True))
+print((False) and (True))
+
+# print((True and True) or (False and True))
+print((True) or (True))
+
+# print((False or True) and (True and False))
+print((True) and (True))
+
+# print((False and False) or (False or True))
+print((False) or (True))
+
+# print((True or False) and (False or True))
+print((False) and (True))
+
+# print((True and False) or (True and False))
+print((True) or (True))
+
+# print((True and True) or (True and False))
+print((False) or (True))
+
+# print((False and False) or (True or bool(5)))
+print((False) or (True))
+
+# print((True or True) and (False and False))
+print((True) and (False))
+
+# print((False or True) and (True or True))
+print((True) and (True))
+
+# print((True and False) or (True and False))
+print((True) or (True))
